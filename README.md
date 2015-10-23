92bdf66bfe81393631b1ce6d06f3050dc3318b5f
PrestonPhro <prestonfrom@gmail.com>
2015-10-23 09:15:38 +0900

    Scales4All v0.01
    - Main loop
    - Scale class (find scale, fix scale for writing on a staff)
    - Fix flat scale needs implementation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Scales4All
+A simple program for finding scales
+
+This console program will give the user a scale in a designated mode with with a root note set by the user.
+Written in C++/Visual Studio 2015
---
+++ Scale.hpp
@@ -0,0 +1,52 @@
+#ifndef SCALE_HPP
+#define SCALE_HPP
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+
+struct ScaleNotes {
+	int ident;
+	std::string note;
+};
+
+struct ScaleSteps {
+	std::string scaleName;
+	std::vector<int> steps;
+};
+
+class Scale {
+protected:
+	std::vector<ScaleSteps> scaleStepsVec;
+	std::vector<ScaleNotes> notes;
+	std::vector<std::string> chromScale = { "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" };
+	std::vector<std::string> altChromScale = { "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab" };
+	std::vector<std::string> currentScale;
+	
+	
+public:
+	bool loadScales();
+	std::vector<std::string> makeScale(std::string root, std::string inputScale);
+	
+	bool clearScale() { 
+		currentScale.erase(currentScale.begin(), currentScale.end()); 
+		return true;
+	}
+
+	std::vector<int> setSelectedScale(std::string inputScale);
+
+	std::vector<std::string> fixScale(std::vector<std::string> inputScale);
+	std::vector<std::string> fixScaleSharp(std::vector<std::string> inputScale);
+
+	std::vector<std::string> getScaleNames() {
+		std::vector<std::string> names;
+		for (int i = 0; i < scaleStepsVec.size(); i++) {
+			names.push_back(scaleStepsVec.at(i).scaleName);
+		}
+		return names;
+	}
+
+};
+ 
+#endif
